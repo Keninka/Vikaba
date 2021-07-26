977f0ecbdf11ec1ef81eae316ae735e3546d5c4a
Kenin <cylon992@gmail.com>
2021-07-26 22:43:49 +0700

    Added navigation and thread catalogues

---
+++ Vikaba/Data/Database/Comments.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Vikaba.Data.Database
+{
+    public static class Comments
+    {
+        public static List<BoardThread> CommentList = new List<BoardThread>();
+        
+    }
+    
+    
+}
